5904c32c621ecc2b2bc26aa7c04a79e4bec5d07f
tomohiro-iwa <tomo_develop_singularity_111@ezweb.ne.jp>
2020-06-13 16:24:27 +0900

    inital commit

---
+++ src/main/kotlin/iwa/Main.kt
@@ -0,0 +1,36 @@
+package iwa
+
+import edu.stanford.nlp.ling.CoreAnnotations
+import edu.stanford.nlp.pipeline.Annotation
+import edu.stanford.nlp.pipeline.StanfordCoreNLP
+import edu.stanford.nlp.trees.*
+
+import java.util.*
+
+fun main(args: Array<String>){
+  var props:Properties = Properties()
+  props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse")
+  // use faster shift reduce parser
+  //props.setProperty("parse.model", "edu/stanford/nlp/models/srparser/englishSR.ser.gz")
+  props.setProperty("parse.maxlen", "100")
+  props.setProperty("parse.binaryTrees", "true")
+  // set up Stanford CoreNLP pipeline
+  var pipeline :StanfordCoreNLP = StanfordCoreNLP(props)
+  // build annotation for a review
+  var annotation :Annotation = Annotation("The small red car turned very quickly around the corner.")
+  // annotate
+  pipeline.annotate(annotation)
+  // get tre    //Set<Constituent> treeConstituents = tree.constituents(new LabeledScoredConstituentFactory());
+    //for (Constituent constituent : treeConstituents) {
+    //  if (constituent.label() != null &&
+    //      (constituent.label().toString().equals("VP") || constituent.label().toString().equals("NP"))) {
+    //    System.err.println("found constituent: "+constituent.toString());
+    //    System.err.println(tree.getLeaves().subList(constituent.start(), constituent.end()+1));
+    //  }
+    //}
+e
+  var tree :Tree = annotation.get(CoreAnnotations.SentencesAnnotation::class.java).get(0).get(TreeCoreAnnotations.TreeAnnotation::class.java)
+  println(tree)
+
+}
+
